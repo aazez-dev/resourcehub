ae2bc8a11169cea4190078ea15003c96194e2728
Abdulaziz3hmed <abdulaziz.elquza@gmail.com>
2026-06-15 04:24:28 +0300

    Initilase Soring Boot setup

---
+++ src/test/java/com/aazizdev/resourcehub/ResourcehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aazizdev.resourcehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
